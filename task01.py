354625dc0dcec46cd360e1346d8489a90c500441
aboodetk <TK.Al-Tekreeti@dal.ca>
2017-10-20 09:45:11 -0700

    task01 code still in progress

--- task01.py
+++ task01.py
@@ -1,1 +1,24 @@
+###first step opening the books
+file1=open('Book1.txt')
+file2=open('Book2.txt')
+file3=open('Book3.txt')
 
+###creating a function 
+def longest_word():
+
+###creating an empty string that would be used to have the biggest word
+ l1=""
+
+###looping through the Book1
+ for line in file1:
+  for word in line.split():
+   
+
+
+
+
+
+
+
+
+longest_word()
